6b606784ec43cf057e5a17a3b2630091f3a6677e
Arthur de Barros <arthurbotelho1@hotmail.com>
2024-12-15 16:48:14 -0300

    Tela de chat primeira implementação

--- lib/components/menu.dart
+++ lib/components/menu.dart
@@ -1,3 +1,4 @@
+import 'package:auto_hub/screens/chat_screen.dart';
 import 'package:auto_hub/screens/home_screen.dart';
 import 'package:auto_hub/screens/login_screen.dart';
 import 'package:auto_hub/screens/my_announcements_screen.dart';
@@ -45,6 +46,18 @@ class Menu extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                   builder: (context) => MyAnnouncementsScreen(user: user),
+                ),
+              );
+            },
+
+          ),
+          ListTile(leading: const Icon(Icons.chat_bubble),
+            title: const Text('Minhas Conversas'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyChatsScreen(user: user),
                 ),
               );
             },
