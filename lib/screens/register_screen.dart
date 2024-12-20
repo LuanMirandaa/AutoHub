dad2c2fe38dfc6e5bbbb5b99863be72b94f3383e
Edniz Silva <87082825+EdiSil@users.noreply.github.com>
2024-12-20 00:58:52 -0300

    Update register_screen.dart
    1 - Foi tornamos a instância de AuthService final para atender aos requisitos da anotação @immutable.
    2 - Uso de BuildContext em lacunas assíncronas: Foi Adicionado verificações if (context.mounted) antes de qualquer uso de BuildContext após operações assíncronas, garantindo assim que o contexto ainda é válido antes de manipular a interface do usuário.
    3 - Foi ajustado o tratamento de erros e mensagens de feedback para o usuário usando o ScaffoldMessenger com verificações apropriadas.

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -9,7 +9,7 @@ class RegisterScreen extends StatelessWidget {
   final TextEditingController _confirmarSenhaController = TextEditingController();
   final TextEditingController _nomeController = TextEditingController();
 
-  AuthService authServicer = AuthService();
+  final AuthService authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -17,67 +17,87 @@ class RegisterScreen extends StatelessWidget {
       body: Container(
         color: Colors.white,
         child: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Container(
-              padding: const EdgeInsets.all(20),
-              margin: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                children: [Image.asset('assets/images/logo.png', width: 250, height: 250,),
-                  TextField(
-                    controller: _nomeController,
-                    decoration: const InputDecoration(hintText: 'Nome'),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(hintText: 'E-mail'),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextField(
-                    obscureText: true,
-                    controller: _senhaController,
-                    decoration: const InputDecoration(hintText: 'Senha'),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextField(
-                    obscureText: true,
-                    controller: _confirmarSenhaController,
-                    decoration: const InputDecoration(hintText: 'Confirmar Senha'),
-                  ),
-                  const SizedBox(
-                    height: 30,),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/logo.png',
+                      width: 250,
+                      height: 250,
+                    ),
+                    TextField(
+                      controller: _nomeController,
+                      decoration: const InputDecoration(hintText: 'Nome'),
+                    ),
+                    const SizedBox(height: 30),
+                    TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                    ),
+                    const SizedBox(height: 30),
+                    TextField(
+                      obscureText: true,
+                      controller: _senhaController,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                    ),
+                    const SizedBox(height: 30),
+                    TextField(
+                      obscureText: true,
+                      controller: _confirmarSenhaController,
+                      decoration: const InputDecoration(hintText: 'Confirmar Senha'),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_senhaController.text == _confirmarSenhaController.text) {
+                          final String? erro = await authService.cadastrarUsuario(
+                            email: _emailController.text,
+                            senha: _senhaController.text,
+                            nome: _nomeController.text,
+                          );
 
-                  ElevatedButton(onPressed: (){
-                    if (_senhaController.text == _confirmarSenhaController.text){
-                        authServicer.cadastrarUsuario(email: _emailController.text, senha: _senhaController.text, nome: _nomeController.text).then((String? erro){
-
-                          if (erro != null){
-                            final snackBar = SnackBar(content: Text(erro), backgroundColor: Colors.red);
-                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                          } else{
-                            Navigator.pop(context);
+                          if (erro != null) {
+                            if (context.mounted) {
+                              final snackBar = SnackBar(
+                                content: Text(erro),
+                                backgroundColor: Colors.red,
+                              );
+                              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                            }
+                          } else {
+                            if (context.mounted) {
+                              Navigator.pop(context);
+                            }
                           }
-                        });
-                    }else{
-                      const snackBar = SnackBar(content: Text('As senhas não correspondem'), backgroundColor: Colors.red,);
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    }
-                  }, child: const Text('Cadastrar'))
-                ],
-              )),
-        ])),
+                        } else {
+                          if (context.mounted) {
+                            const snackBar = SnackBar(
+                              content: Text('As senhas não correspondem'),
+                              backgroundColor: Colors.red,
+                            );
+                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          }
+                        }
+                      },
+                      child: const Text('Cadastrar'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
